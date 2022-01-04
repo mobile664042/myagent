97d5004ae75a34cfbee3535ddfc4a7e3b117eee0
myboo <624030518@qq.com>
2022-01-04 15:54:55 +0800

    Update SimpleMain.java
    修复与arthas冲突bug

--- src/main/java/myagent/boot/SimpleMain.java
+++ src/main/java/myagent/boot/SimpleMain.java
@@ -1,9 +1,25 @@
 package myagent.boot;
 
+import java.io.File;
 import java.lang.instrument.Instrumentation;
+import java.security.CodeSource;
+import java.util.jar.JarFile;
 
 public class SimpleMain {
 	public static void premain(String agentArguments, Instrumentation instrumentation) {
+		try {
+			loadAgent(instrumentation);
+		} catch (Exception e) {
+			e.printStackTrace();
+			myagent.edit.MyPrint.println("add agent myself to classpath failed !!!!");
+		}
 		instrumentation.addTransformer(new SimpleTransformer());
 	}
+	
+	private static void loadAgent(Instrumentation instrumentation) throws Exception {
+		CodeSource codeSource = myagent.edit.MyPrint.class.getProtectionDomain().getCodeSource();
+		File agentJarFile = new File(codeSource.getLocation().toURI().getSchemeSpecificPart());
+		instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(agentJarFile));
+		myagent.edit.MyPrint.println("add agent myself to classpath " + agentJarFile.getAbsolutePath());
+        }
 }
